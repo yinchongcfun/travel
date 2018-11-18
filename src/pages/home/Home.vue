7ee4cfdf92445e614fe1521d3942d6112a35c9cf
cfun <1606548133@qq.com>
2018-11-18 20:41:43 +0800

    增加一点css

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="home">home</div>
+        <br>
+        <router-link to="/list">列表页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+    div{
+        color: red;
+    }
+
+    .home{
+        font-size: 30px;
+    }
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
+
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+        path: '/list',
+        name: 'List',
+        component: List
     }
   ]
 })
